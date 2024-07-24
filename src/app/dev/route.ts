3b4496ca45ee8fdad85b0a654d0aeb7d99e3e32d
vpittamp <vinod@pittampalli.com>
2024-07-24 14:13:21 +0000

    chore: Update npm dependencies for Microsoft Graph API integration

--- src/app/dev/route.ts
+++ src/app/dev/route.ts
@@ -1,8 +1,12 @@
+import "isomorphic-fetch"; // or import the fetch polyfill you installed
+import { Client } from "@microsoft/microsoft-graph-client";
+
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export const GET = auth(async ({ auth }) => {
-    const data = await fetch("https://graph.microsoft.com/v1.0/me/messages", {
+    Client.init({});
+    const data = await fetch("https://api.fabric.microsoft.com/v1/workspaces/8986808f-6c68-4a91-b3c4-1a38bfa8d7e1/graphqlapis/0c5175ba-3eb0-4e16-ba31-120722369a4b/graphql", {
         headers: {
             Authorization: `Bearer ${auth?.accessToken}`,
         },
